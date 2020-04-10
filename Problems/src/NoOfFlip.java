bbe45b29ca17e15a73989ffe0e517c3e03deb942
smit kadvani <14it046@charusat.edu.in>
2020-04-10 21:18:39 +0530

    Documentation added

--- Problems/src/NoOfFlip.java
+++ Problems/src/NoOfFlip.java
@@ -1,4 +1,29 @@
+/*
 
+Flip
+Asked in:  
+VMWare
+You are given a binary string(i.e. with characters 0 and 1) S consisting of characters S1, S2, …, SN. In a single operation, you can choose two indices L and R such that 1 ≤ L ≤ R ≤ N and flip the characters SL, SL+1, …, SR. By flipping, we mean change character 0 to 1 and vice-versa.
+
+Your aim is to perform ATMOST one operation such that in final string number of 1s is maximised. If you don’t want to perform the operation, return an empty array. Else, return an array consisting of two elements denoting L and R. If there are multiple solutions, return the lexicographically smallest pair of L and R.
+
+Notes:
+
+Pair (a, b) is lexicographically smaller than pair (c, d) if a < c or, if a == c and b < d.
+For example,
+
+S = 010
+
+Pair of [L, R] | Final string
+_______________|_____________
+[1 1]          | 110
+[1 2]          | 100
+[1 3]          | 101
+[2 2]          | 000
+[2 3]          | 001
+
+We see that two pairs [1, 1] and [1, 3] give same number of 1s in final string. So, we return
+*/
 import java.util.ArrayList;
 
 /*
@@ -13,7 +38,15 @@ import java.util.ArrayList;
 public class NoOfFlip {
     
     
+    // This is greedy approach to solve this problem
+    /* It involves complexity of o(n^2) 
     
+    This complexity can be further reduced to linear time using modified version of kadane's algorithms
+    
+    
+    */
+    
+    // Greedy approach 
     public static int checkFlip(String A,int i,int j)
     {
         StringBuilder str = new StringBuilder(A);
@@ -61,7 +94,13 @@ public class NoOfFlip {
          
          
          
+     // Using kadane's algorithm
      
+     public static ArrayList<Integer> flip_kadane(String A)
+             
+     {
+         
+     }
     public static void main(String[] args) {
         
         flip("010");
