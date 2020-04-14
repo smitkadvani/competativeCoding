edd48190514f67af069349403829f5d1d5bccd35
smit kadvani <14it046@charusat.edu.in>
2020-04-14 11:21:56 +0530

    Leetcode details added

--- Problems/src/problems/MinStack.java
+++ Problems/src/problems/MinStack.java
@@ -3,6 +3,32 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+
+/*
+  Min Stack
+Solution
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+ 
+
+Example:
+
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> Returns -3.
+minStack.pop();
+minStack.top();      --> Returns 0.
+minStack.getMin();   --> Returns -2.
+
+
+*/
 package problems;
 
 import java.util.ArrayList;
