e11f74031dc81cf33104a896bf3e27ccd5efa6f7
smit kadvani <14it046@charusat.edu.in>
2020-04-21 01:17:04 +0530

    with problem statement

--- Problems/src/problems/MinDistance.java
+++ Problems/src/problems/MinDistance.java
@@ -2,7 +2,27 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+
+
+
  */
+
+/*
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+
+Example:
+
+Input:
+[
+  [1,3,1],
+  [1,5,1],
+  [4,2,1]
+]
+Output: 7
+Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+*/
 package problems;
 
 import static java.lang.Integer.min;
